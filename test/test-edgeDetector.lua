af1da4161f1091ce5d227cd0143e6ecfa248d372
Alfredo Canziani <alfredo.canziani@gmail.com>
2012-10-31 19:35:01 -0400

    Edges extractor fully working
    Online edges extractor (part of the final stereo matching algorithm)
    Tentative of using Clemant's debugger (that does not work with Jit);
    anyway, it does not work yet...

--- test/test-edgeDetector.lua
+++ test/test-edgeDetector.lua
@@ -2,6 +2,7 @@
 
 require 'image'
 require 'edgeDetector'
+--require 'debugger'
 
 -- initialising cameras and required needed packages
 
@@ -12,12 +13,15 @@ height = 300
 fps = 30 
 --dir = 'test_vid_two'
 
-sys.execute(string.format('mkdir -p %s',dir))
+--sys.execute(string.format('mkdir -p %s',dir))
 
 camera1 = image.Camera{idx=1,width=width,height=height,fps=fps}
+
+--[[---------------------------------------------------------------------------
+-- Working with dimensions and histograms
+-------------------------------------------------------------------------------
 iCameraLc = camera1:forward() -- acquiring image from the LEFT camera
 iCameraL = image.rgb2y(iCameraLc)
-
 edges = edgeDetector(iCameraL)
 
 image.display{image = edges, legend = 'Image edges', zoom = 3}
@@ -43,3 +47,26 @@ gnuplot.hist(edges[1],100,-.2,.2)
 gnuplot.title('edges[1]')
 gnuplot.raw('set terminal postscript enhanced "Helvetica" 24')
 gnuplot.plotflush()
+
+--]]---------------------------------------------------------------------------
+-- Removing the background (no variance zones)
+-------------------------------------------------------------------------------
+while true do
+   
+   iCameraLc = camera1:forward() -- acquiring image from the LEFT camera
+   iCameraL = image.rgb2y(iCameraLc) -- turning into a greyscale version
+   edges = edgeDetector(iCameraL)
+
+   cleaned = torch.Tensor(3,298,398)
+   cleaned[1], cleaned[2], cleaned[3] = edges[1], edges[1], edges[1]
+   
+   --if _DEBUG_ then pause() end
+   min = edges[1]:min()
+   edges = edges:abs();
+   cleaned[2][torch.gt(edges,.05)] = min;
+   cleaned[3][torch.gt(edges,.05)] = min;
+
+
+   win = image.display{win = win, image = cleaned, legend = 'Image thresholded', zoom = 3}
+
+end
