df0a771f546a623ac580b7500acd449b964650c2
Alfredo Canziani <alfredo.canziani@gmail.com>
2012-11-16 13:57:42 -0500

    Updated test-edgeDetector.lua since the edgeDetector now requires the kernel size

--- test/test-edgeDetector.lua
+++ test/test-edgeDetector.lua
@@ -4,6 +4,14 @@ require 'image'
 require 'edgeDetector'
 --require 'debugger'
 
+-- Asking to the user what to do
+io.write("\nHere there are provided two different demos\n")
+io.write("  Type 'h' if you want to view the [h]istograms,\n")
+io.write("  type 'l' if you want to view the [l]ive demo (h|l): ")
+
+ans = io.read()
+io.write('\n')
+
 -- initialising cameras and required needed packages
 
 require 'xlua'
@@ -17,56 +25,67 @@ fps = 30
 
 camera1 = image.Camera{idx=1,width=width,height=height,fps=fps}
 
---[[---------------------------------------------------------------------------
--- Working with dimensions and histograms
--------------------------------------------------------------------------------
-iCameraLc = camera1:forward() -- acquiring image from the LEFT camera
-iCameraL = image.rgb2y(iCameraLc)
-edges = edgeDetector(iCameraL)
+if (ans == 'h') then
 
-image.display{image = edges, legend = 'Image edges', zoom = 3}
-image.display{image = iCameraL, legend = 'Greyscale input image', zoom = 3}
-
-print(iCameraL:size())
-print(iCameraL[1]:size())
-print(iCameraL[1]:min())
-print(iCameraL[1]:max())
-
-gnuplot.figure(1)
-gnuplot.hist(iCameraL[1],100)
-gnuplot.title('iCameraL[1]')
-
-print(edges:size())
-print(edges[1]:size())
-print(edges[1]:min())
-print(edges[1]:max())
-
-gnuplot.epsfigure('histEdges.eps')
-gnuplot.figure(2)
-gnuplot.hist(edges[1],100,-.2,.2)
-gnuplot.title('edges[1]')
-gnuplot.raw('set terminal postscript enhanced "Helvetica" 24')
-gnuplot.plotflush()
-
---]]---------------------------------------------------------------------------
--- Removing the background (no variance zones)
--------------------------------------------------------------------------------
-while true do
-   
+   -------------------------------------------------------------------------------
+   -- Working with dimensions and histograms
+   -------------------------------------------------------------------------------
    iCameraLc = camera1:forward() -- acquiring image from the LEFT camera
-   iCameraL = image.rgb2y(iCameraLc) -- turning into a greyscale version
-   edges = edgeDetector(iCameraL)
+   iCameraL = image.rgb2y(iCameraLc)
+   edges = edgeDetector(iCameraL,'3')
 
-   cleaned = torch.Tensor(3,298,398)
-   cleaned[1], cleaned[2], cleaned[3] = edges[1], edges[1], edges[1]
-   
-   --if _DEBUG_ then pause() end
-   min = edges[1]:min()
-   edges = edges:abs();
-   cleaned[2][torch.gt(edges,.05)] = min;
-   cleaned[3][torch.gt(edges,.05)] = min;
+   image.display{image = edges, legend = 'Image edges', zoom = 3}
+   image.display{image = iCameraL, legend = 'Greyscale input image', zoom = 3}
+
+   print(iCameraL:size())
+   print(iCameraL[1]:size())
+   print(iCameraL[1]:min())
+   print(iCameraL[1]:max())
+
+   gnuplot.figure(1)
+   gnuplot.hist(iCameraL[1],100)
+   gnuplot.title('iCameraL[1]')
+
+   print(edges:size())
+   print(edges[1]:size())
+   print(edges[1]:min())
+   print(edges[1]:max())
+
+   gnuplot.figure(2)
+   gnuplot.hist(edges[1],100,-.2,.2)
+   gnuplot.title('edges[1]')
+
+   gnuplot.epsfigure('histEdges.eps')
+   gnuplot.figure(2)
+   gnuplot.hist(edges[1],100,-.2,.2)
+   gnuplot.title('edges[1]')
+   gnuplot.raw('set terminal postscript enhanced "Helvetica" 24')
+   gnuplot.plotflush()
+
+end
+if (ans == 'l') then
+
+   -------------------------------------------------------------------------------
+   -- Removing the background (no variance zones)
+   -------------------------------------------------------------------------------
+   while true do
+
+      iCameraLc = camera1:forward() -- acquiring image from the LEFT camera
+      iCameraL = image.rgb2y(iCameraLc) -- turning into a greyscale version
+      edges = edgeDetector(iCameraL,'3')
+
+      cleaned = torch.Tensor(3,298,398)
+      cleaned[1], cleaned[2], cleaned[3] = edges[1], edges[1], edges[1]
+
+      --if _DEBUG_ then pause() end
+      min = edges[1]:min()
+      edges = edges:abs();
+      cleaned[2][torch.gt(edges,.05)] = min;
+      cleaned[3][torch.gt(edges,.05)] = min;
 
 
-   win = image.display{win = win, image = cleaned, legend = 'Image thresholded', zoom = 3}
+      win = image.display{win = win, image = cleaned, legend = 'Image thresholded', zoom = 3}
+
+   end
 
 end
