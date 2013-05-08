912ac0a459554ae497b5b262b62130420b2dc34b
Alfredo Canziani <alfredo.canziani@gmail.com>
2013-05-08 11:51:08 -0400

    Added test on not existing $DATASET environment varialbe

--- usefulFunctions.lua
+++ usefulFunctions.lua
@@ -9,6 +9,25 @@ function ls(path) return sys.split(sys.ls(path),'\n') end
 
 -- datasetPath - getting the environmental $DATASET variable
 function datasetPath()
+   local ds
    ds = os.getenv("DATASETS")
+   if not ds then
+      io.write [[
+
+******************************************************************************
+   WARNING - $DATASET environment variable is not defined
+******************************************************************************
+   Please define an environment $DATASET variable
+
+      $ export DATASET='datasetDirectoryOnCurrentMachine'
+
+   and add it to your <.bashrc> configuration file in order to do not
+   visualise this WARNING message again
+******************************************************************************
+
+Please, type the dataset directory path for the current machine:
+]]
+      ds = io.read()
+   end
    return ds
 end
