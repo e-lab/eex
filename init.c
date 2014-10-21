254de083b7d681773c57c15996672fc40a9309bc
Jonghoon Jin <jhjin0@gmail.com>
2014-10-21 12:41:12 -0400

    Bug Fix: duplicated headers are removed
    While cleaning up branches, headers remain as duplicates.

--- init.c
+++ init.c
@@ -11,15 +11,10 @@
 #include "THGenerateFloatTypes.h"
 #include "generic/SpatialSAD.c"
 #include "THGenerateFloatTypes.h"
-#include "generic/SpatialSADMap.c"
-#include "THGenerateFloatTypes.h"
 
 #include "generic/SpatialMaxMap.c"
 #include "THGenerateFloatTypes.h"
 #include "generic/SpatialSADMap.c"
-#include "THGenerateFloatTypes.h"
-
-#include "generic/SpatialMaxMap.c"
 #include "THGenerateFloatTypes.h"
 
 DLL_EXPORT int luaopen_libeex(lua_State *L)
